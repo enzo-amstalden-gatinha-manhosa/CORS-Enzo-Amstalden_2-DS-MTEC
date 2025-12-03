74361de8a1995df56704c7449dc9528326877792
Enzo Amstalden <13enzomeida@gmail.com>
2025-12-03 13:50:04 -0300

    Projeto CORS - Frontend e Backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 8080;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET',
+    optionsSuccessStatus: 200
+
+};
+
+app.use(cors(corsOptions));
+
+app.get('/api/mensagem', (req, res) => {
+    res.json({ data: "Mensagem secreta da API em 8080! - COM PERMISSÃO CORS" });
+});
+
+ app.listen(PORT, () => {
+    console.log(`API Backend rodando em: http://localhost:${PORT}`);
+    console.log('AGORA PERMITINDO acesso de: http://localhost:3000');
+ });
